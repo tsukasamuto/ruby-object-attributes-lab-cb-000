d062a63d493fd7dec9a07c77ade5879697599e45
Tsukasa Muto <tsukasa610@gmail.com>
2017-09-17 15:05:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,4 @@
 class Dog
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
 
   def name=(new_name)
     @name = new_name
@@ -19,4 +15,5 @@ class Dog
   def breed
     @breed
   end
+  
 end
